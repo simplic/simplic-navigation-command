0c219d7d8e0f2b2b8d54cdfd0137e3f155bce105
beggers <be@spiegelburg.net>
2020-04-01 21:08:19 +0200

    Add CI/CD

--- src/Simplic.Navigation.Command.UI/package.bat
+++
@@ -1,3 +0,0 @@
-del *.nupkg
-nuget pack Simplic.Navigation.Command.UI.csproj -properties Configuration=Debug
-nuget push *.nupkg -Source http://simplic.biz:10380/nuget
--- src/Simplic.Navigation.Command/package.bat
+++
@@ -1,3 +0,0 @@
-del *.nupkg
-nuget pack Simplic.Navigation.Command.csproj -properties Configuration=Debug
-nuget push *.nupkg -Source http://simplic.biz:10380/nuget
